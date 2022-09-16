6d3efec1377f7eb8c482887311933b39854e444a
iamdayo <ppldayo@gmail.com>
2022-09-16 17:03:42 +0100

    deleted favicon

--- src/App.js
+++ src/App.js
@@ -2,18 +2,13 @@
 import Footer from "./components/Footer";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
-import { Helmet } from "react-helmet";
+
 
 
 function App() {
   return (
     <div>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Food App</title>
-        <link rel="canonical" href="http://mysite.com/example" />
-        <meta name="description" content="Food application" />
-      </Helmet>
+      
       <Navbar />
       <Hero />
       <Footer/>
